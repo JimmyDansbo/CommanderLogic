463e7e75bf13925ab1bf97495f15dc9bfa43c35b
Jimmy Dansbo <jimmy@dansbo.dk>
2022-10-11 14:53:05 +0200

    Modified drawing.asm and build.sh to for new external font file build.sh now buildes the font as a separate binary fil drawing.asm loads the font binary directly into VRAM using kernal functions

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 rm -rf *.PRG
 rm -rf *.prg
 acme -f cbm -o DRAWING.PRG drawing.asm
+acme -f cbm -o PARTFONT.BIN partialfont.asm
 acme -f cbm -o LEVELS.BIN levels.asm
 acme -f cbm -o COMLOG.PRG commanderlogic.asm
