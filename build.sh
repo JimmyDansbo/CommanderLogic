e4c724c2706a2c5edee7cc3ac81cd06e58c07f6c
Jimmy Dansbo <jimmy@dansbo.dk>
2022-10-19 16:30:45 +0200

    Not compiling drawing.asm as asc2vera.ct has been removed Look at https://github.com/dansbo/proto2/ for ar version of asc2vera.ct

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 rm -rf *.PRG
 rm -rf *.prg
 zsbg/build.sh
-acme -f cbm -o DRAWING.PRG drawing.asm
 acme -f cbm -o PARTFONT.BIN partialfont.asm
 acme -f cbm -o LEVELS.BIN levels.asm
 acme -f cbm -o COMLOG.PRG commanderlogic.asm
