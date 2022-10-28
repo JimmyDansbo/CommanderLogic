64ffd10d2d25237f2f74f0a967085db52398e625
Jimmy Dansbo <jimmy@dansbo.dk>
2022-10-28 16:43:46 +0200

    Added a bit of output to the build script

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+echo "Removing files..."
 rm -rf *.PRG
 rm -rf *.prg
+rm -rf ZSBG.BIN PARTFONT.BIN LEVELS.BIN
+echo "Building ZSBG.BIN"
 zsbg/build.sh
+echo "Building PARTFONT.BIN"
 acme -f cbm -o PARTFONT.BIN partialfont.asm
+echo "Building LEVELS.BIN"
 acme -f cbm -o LEVELS.BIN levels.asm
+echo "Building COMLOG.PRG"
 acme -f cbm -o COMLOG.PRG commanderlogic.asm
